1b9bcd99990f22b8f9843eb391cf40962abe8bb8
Henry Gabriel <63942305+HenryGabriel-2407@users.noreply.github.com>
2024-11-28 13:50:46 -0300

    :D

--- fast_two_sqlAlchemy/code_2.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, text
-
-engine = create_engine('sqlite:///database.db', echo=True)
-
-with engine.connect() as con:
-    sql = text("SELECT * FROM users")
-    result = con.execute(sql)
